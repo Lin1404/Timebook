a11075f108e42663da2b520cf9c7b729f523c009
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-05-02 18:20:10 -0400

    add RequestMapping for week, month year

--- src/main/java/com/timebook/timebook/controllers/TimebookController.java
+++ src/main/java/com/timebook/timebook/controllers/TimebookController.java
@@ -1,7 +1,19 @@
 package com.timebook.timebook.controllers;
 
+import com.timebook.timebook.events.event;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import java.util.List;
 
 @RestController
 public class TimebookController {
@@ -11,4 +23,18 @@ public class TimebookController {
         return "Welcome to Timebook!";
     }
     
+    @RequestMapping(value = "/v1/calendar/week/{date}/{email}", method = RequestMethod.GET)
+    public Integer weekly(@PathVariable ("date") Integer date, @PathVariable ("email") String email){
+        return date;
+    }
+
+    @GetMapping("/v1/calendar/month/{date}/{email}")
+    List<String> monthly() {
+        return List.of("Hello", "monthly");
+    }
+
+    @GetMapping("/v1/calendar/annual/{date}/{email}")
+    List<String> annually() {
+        return List.of("Hello", "annually");
+    }
 }
