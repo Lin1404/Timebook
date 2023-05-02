64314da1efd94bdd42216fa381b431a61bf79f8f
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-05-02 16:21:11 -0400

    update gitignore file

--- src/main/java/com/timebook/timebook/Events/Event.java
+++ src/main/java/com/timebook/timebook/Events/Event.java
@@ -1,42 +1,42 @@
-package com.timebook.timebook.Events;
+// package com.timebook.timebook.Events;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+// import jakarta.annotation.Generated;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
 
 
-import java.util.Objects;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
+// import java.util.Objects;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
 
-@Entity
-public class Event {
-    private @Id @GeneratedValue long id;
-    private String email;
-    private String title;
-    private String descriptions;
-    private String startDateTime;
-    private String endDateTime;
-    private String priority;
+// @Entity
+// public class Event {
+//     private @Id @GeneratedValue long id;
+//     private String email;
+//     private String title;
+//     private String descriptions;
+//     private String startDateTime;
+//     private String endDateTime;
+//     private String priority;
 
-    Event(){}
+//     Event(){}
 
-    Event(
-        long id, 
-        String email, 
-        String title, 
-        String descriptions, 
-        String startDateTime,
-        String endDateTime,
-        String priority
-        ){
-            this.id = id;
-            this.email = email;
-            this.title = title;
-            this.descriptions = descriptions;
-            this.startDateTime = startDateTime;
-            this.endDateTime = endDateTime;
-            this.priority = priority;
-        }
-}
+//     Event(
+//         long id, 
+//         String email, 
+//         String title, 
+//         String descriptions, 
+//         String startDateTime,
+//         String endDateTime,
+//         String priority
+//         ){
+//             this.id = id;
+//             this.email = email;
+//             this.title = title;
+//             this.descriptions = descriptions;
+//             this.startDateTime = startDateTime;
+//             this.endDateTime = endDateTime;
+//             this.priority = priority;
+//         }
+// }
