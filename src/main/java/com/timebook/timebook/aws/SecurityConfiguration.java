c225d1c8632c424fddc4d68da7d5594485d4707e
weixiang237 <87122779+weixiang237@users.noreply.github.com>
2023-05-11 20:50:36 -0400

    integrated cognito

---
+++ src/main/java/com/timebook/timebook/aws/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.timebook.timebook.aws;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Autowired
+	private AwsCognitoJwtAuthFilter awsCognitoJwtAuthenticationFilter;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers(headers -> headers.cacheControl());
+        http.csrf(withDefaults())
+                .authorizeHttpRequests()
+                //.requestMatchers("**/health").permitAll()
+                .requestMatchers("/v1/**").authenticated()
+                //.anyRequest().authenticated()
+                .and()
+                .addFilterBefore(awsCognitoJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
